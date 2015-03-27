06cdb971ef9e43114e9aa899ca348abda02f5b28
Ramon Lopez <ramon.lopez@aon.at>
2015-03-27 09:17:15 +0100

    Market UnitTests can now connect to spaces properly

--- Market/src/test/MarketInitTests.java
+++ Market/src/test/MarketInitTests.java
@@ -1,22 +1,14 @@
-import ac.at.tuwien.sbc.domain.configuration.CommonRabbitConfiguration;
 import ac.at.tuwien.sbc.domain.configuration.CommonSpaceConfiguration;
-import ac.at.tuwien.sbc.market.MarketApp;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mozartspaces.capi3.AnyCoordinator;
 import org.mozartspaces.capi3.FifoCoordinator;
 import org.mozartspaces.core.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
@@ -24,8 +16,6 @@ import java.util.ArrayList;
 /**
  * Created by shady on 26/03/15.
  */
-
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CommonSpaceConfiguration.class)
 @ActiveProfiles("space")
@@ -41,10 +31,18 @@ public class MarketInitTests {
     @Qualifier("defaultContainer")
     ContainerReference defaultContainer;
 
+    @Autowired
+    @Qualifier("investorDepotContainer")
+    ContainerReference investorDepotContainer;
+
+    @Autowired
+    @Qualifier("orderContainer")
+    ContainerReference orderContainer;
+
     @Before
     public void fillSpace(){
         try {
-            capi.write(defaultContainer, new Entry("Market enters the space!"));
+            capi.write(defaultContainer, MzsConstants.RequestTimeout.TRY_ONCE,null, new Entry("Market entered space"));
 
         } catch (MzsCoreException e) {
             e.printStackTrace();
@@ -53,10 +51,13 @@ public class MarketInitTests {
 
     @Test
     public void readEntries() throws Exception {
-//        capi.write(defaultContainer, new Entry("mooooooore!"));
-//        ArrayList<Entry> resultEntries = capi.read(defaultContainer, FifoCoordinator.newSelector(0),10,null);
-//        Assert.assertTrue(!resultEntries.isEmpty());
-//        System.out.println("Entry read: " + resultEntries.get(0));
+//        ArrayList<FifoCoordinator.FifoSelector> selectors = new ArrayList<>();
+//        selectors.add(FifoCoordinator.newSelector(MzsConstants.Selecting.COUNT_ALL));
+//        ArrayList<String> resultEntries = capi.read(defaultContainer, FifoCoordinator.newSelector(1), MzsConstants.RequestTimeout.INFINITE, null);
+
+        ArrayList<Entry> resultEntries = capi.read(investorDepotContainer, FifoCoordinator.newSelector(1),10,null);
+        Assert.assertTrue(!resultEntries.isEmpty());
+        System.out.println("Entry read: " + resultEntries.get(0));
     }
 
 
